6c21a2f095c2ad051ce935f29e7ddef51baf171b
William Dillon <hpux735@gmail.com>
2012-04-29 09:59:42 -0700

    Initial Commit

---
+++ Cocoa-rtl-sdr/CRTLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CRTLAppDelegate.h
+//  Cocoa-rtl-sdr
+//
+//  Created by William Dillon on 4/29/12.
+//  Copyright (c) 2012 Oregon State University (COAS). All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CRTLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Cocoa-rtl-sdr/CRTLAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  CRTLAppDelegate.m
+//  Cocoa-rtl-sdr
+//
+//  Created by William Dillon on 4/29/12.
+//  Copyright (c) 2012 Oregon State University (COAS). All rights reserved.
+//
+
+#import "CRTLAppDelegate.h"
+
+@implementation CRTLAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
