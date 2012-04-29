fe1d797fac2077d681904dd8510e0e86bbddf180
William Dillon <hpux735@gmail.com>
2012-04-29 10:18:56 -0700

    Added a framework for the rtl-sdr code, and moved it from the application to the framework

---
+++ rtl-sdr/rtl-sdr/rtl_sdr.h
@@ -0,0 +1,13 @@
+//
+//  rtl_sdr.h
+//  rtl-sdr
+//
+//  Created by William Dillon on 4/29/12.
+//  Copyright (c) 2012 Oregon State University (COAS). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface rtl_sdr : NSObject
+
+@end
---
+++ rtl-sdr/rtl-sdr/rtl_sdr.m
@@ -0,0 +1,23 @@
+//
+//  rtl_sdr.m
+//  rtl-sdr
+//
+//  Created by William Dillon on 4/29/12.
+//  Copyright (c) 2012 Oregon State University (COAS). All rights reserved.
+//
+
+#import "rtl_sdr.h"
+
+@implementation rtl_sdr
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
